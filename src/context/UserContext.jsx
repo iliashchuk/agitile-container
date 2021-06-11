81ccbc06b017a0c17a48284b4fe0032593be3860
Stanislav Iliashchuk <stanislav.iliashchuk@eleks.com>
2021-06-11 04:14:06 +0300

    Fix a critical bug

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -6,8 +6,6 @@ export const UserContext = createContext({});
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState();
 
-  // test comment
-
   const getUser = useCallback((githubToken) => {
     const getUser = async () => {
       const authenticatedOcto = new Octokit({ auth: githubToken });
